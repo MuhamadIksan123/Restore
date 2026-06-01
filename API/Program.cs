46b42c2acc22a1046f2fb761befaae08ec67c99c
MuhamadIksan123 <muhamadiksan351@gmail.com>
2026-06-01 16:35:20 +0700

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,11 +10,16 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
+
 app.MapControllers();
 
 DbInitializer.InitDb(app);
